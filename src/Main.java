da8010f669def547cbecf21ea06fb90f5afc866a
zulpikar321 <zulpikarzulay@gmail.com>
2020-11-12 07:46:35 -0800

    final work

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello");
-      // B21
+        // B21
+        //  B21 IS dedicated
     }
 }
